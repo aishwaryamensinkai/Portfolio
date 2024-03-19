2e70a5530c4d4389b1ba063d9e0bf0a454e93fa1
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-03-18 23:57:13 -0400

    Added images

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
 import './index.scss'
 
 const Sidebar = () => {
-  return <>Hello from Sidebar</>
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/"></Link>
+    </div>
+  )
 }
 
 export default Sidebar
