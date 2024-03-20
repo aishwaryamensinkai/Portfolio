3c27bfe68f8290fdde1f5b929610a75a4eb055ad
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-03-19 22:10:29 -0400

    Working on Sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,47 @@
-import { Link } from 'react-router-dom'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
     <div className="nav-bar">
-      <Link className="logo" to="/"></Link>
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img src={LogoSubtitle} alt="logo" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/about"
+          className="about-link"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/contact"
+          className="contact-link"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a>
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
