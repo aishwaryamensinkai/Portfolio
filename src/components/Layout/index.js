210bedf46c0fa35291a4322b5fc84353b560771a
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-05-24 23:42:22 -0400

    All websites are resoponsive

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,10 +7,13 @@ const Layout = () => {
     <div className="App">
       <Sidebar />
       <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
-
+        <span className="tags top-tags">
+          &lt;html&gt;
+          <br />
+          <span className="top-tag-html">&lt;body&gt;</span>
+        </span>
         <Outlet />
-        <span className="tags bottom-tags">
+        <span className="tags">
           &lt;/body&gt;
           <br />
           <span className="bottom-tag-html">&lt;/html&gt;</span>
