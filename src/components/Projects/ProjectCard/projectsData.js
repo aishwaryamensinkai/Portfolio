ea728c72019015debd500077fcd7e9fb331890c8
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-07-23 19:24:47 -0400

    Added Projects

--- src/components/Projects/ProjectCard/projectsData.js
+++ src/components/Projects/ProjectCard/projectsData.js
@@ -1,6 +1,71 @@
 const projectsData = [
   {
     type: 'WEB-APP',
+    title: 'Brooklyn Stoop Sale',
+    image: 'sale',
+    link: 'https://aishwaryamensinkai.github.io/Brooklyn-Stoop/',
+    source: 'https://github.com/aishwaryamensinkai/Brooklyn-Stoop',
+  },
+  {
+    type: 'WEB-APP',
+    title: 'TMDB Movies',
+    image: 'movie',
+    link: '  https://aishwaryamensinkai.github.io/tmdb-Movies/#/',
+    source: 'https://github.com/aishwaryamensinkai/tmdb-Movies',
+  },
+  {
+    type: 'WEB-APP',
+    title: 'Terminal Portfolio',
+    image: 'terminal',
+    link: 'https://aishwaryamensinkai.github.io/Terminal-Portfolio/',
+    source: 'https://github.com/aishwaryamensinkai/Terminal-Portfolio',
+  },
+  {
+    type: 'WEB-APP',
+    title: 'Github Readme',
+    image: 'readme',
+    link: 'https://github.com/aishwaryamensinkai',
+    source: 'https://github.com/aishwaryamensinkai/aishwaryamensinkai',
+  },
+  {
+    type: 'WEB-APP',
+    title: 'FlickerImages',
+    image: 'flicker',
+    link: 'https://aishwaryamensinkai.github.io/FlickrImages/firstpage',
+    source: 'https://github.com/aishwaryamensinkai/FlickrImages',
+  },
+  {
+    type: 'WEB-APP',
+    title: 'Fitness App',
+    image: 'fitness',
+    link: 'https://aishwaryamensinkai.github.io/Fitness-App/',
+    source: 'https://github.com/aishwaryamensinkai/Fitness-App',
+  },
+
+  {
+    type: 'WEB-APP',
+    title: 'VCall',
+    image: 'vcall',
+    link: 'https://aishwaryamensinkai.github.io/Video-Call/',
+    source: 'https://github.com/aishwaryamensinkai/Video-Call',
+  },
+
+  {
+    type: 'WEB-APP',
+    title: 'Abhishek`s Portfolio',
+    image: 'ap',
+    link: 'https://prasunabhi.github.io/Portfolio/',
+    source: 'https://github.com/aishwaryamensinkai/AP-Portfolio',
+  },
+  {
+    type: 'WEB-APP',
+    title: 'Karthik`s Portfolio',
+    image: 'kp',
+    link: 'https://kenni001.github.io/Personal_Portfolio/',
+    source: 'https://github.com/aishwaryamensinkai/Kportfolio',
+  },
+  {
+    type: 'WEB-APP',
     title: 'Weather Application',
     image: 'weatherapp',
     link: 'https://aishwaryamensinkai.github.io/Weather-Application/',
@@ -22,23 +87,15 @@ const projectsData = [
   },
   {
     type: 'WEB-APP',
-    title: 'Github Readme',
-    image: 'readme',
-    link: 'https://github.com/aishwaryamensinkai',
-    source: 'https://github.com/aishwaryamensinkai/aishwaryamensinkai',
-  },
-  {
-    type: 'WEB-APP',
-    title: 'FlickerImages',
-    image: 'flicker',
-    link: 'https://aishwaryamensinkai.github.io/FlickrImages/firstpage',
-    source: 'https://github.com/aishwaryamensinkai/FlickrImages',
-  },
-  {
-    type: 'WEB-APP',
     title: 'TradeMaster',
     image: 'trademaster',
     source: 'https://github.com/aishwaryamensinkai/TradeMaster',
+  },
+  {
+    type: 'WEB-APP',
+    title: 'Predicting Response Time of Paris Fire Brigade Vehicles',
+    image: 'dma',
+    source: 'https://github.com/aishwaryamensinkai/Data-Mining-and-Analysis',
   },
 ]
 
